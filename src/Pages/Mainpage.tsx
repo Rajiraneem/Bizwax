67d1ea1cb1d5280c5946464add40161cb7bc813d
Meehad <107790538+Meehad@users.noreply.github.com>
2025-04-29 20:08:14 +0530

    username params for analytics

--- src/Pages/Mainpage.tsx
+++ src/Pages/Mainpage.tsx
@@ -1,4 +1,4 @@
-import { lazy, Suspense, Fragment, memo } from 'react';
+import { lazy, Suspense, Fragment, memo, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import LoadingAnimation from '../Components/LoadingAnimation/LoadingAnimation';
 
@@ -10,15 +10,44 @@ const Benifit = lazy(() => import('../Components/Benifit/Benifit'));
 const Footer = lazy(() => import('../Components/Footer/Footer'));
 
 const Mainpage = () => {
-  const { name = "Guest" } = useParams(); // Get dynamic name from URL
+    // rawParam will look like "amal&1234567890"
+    const { name: rawParam = "Guest&" } = useParams();
+    // split into [userName, phone]; if no “&”, phone will be undefined
+    const [userName, customer_number] = rawParam.split('&');
+  
+    useEffect(() => {
+      if (!customer_number) return;
+  
+      const payload = {
+        phone_number: customer_number,
+        body: "hi",
+        button_text: "Book My Spot!"
+      };
+  
+      fetch("https://bizwax.invitechai.com/webhook/bizwax", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(payload)
+      })
+      .then(res => {
+        if (!res.ok) throw new Error(`HTTP ${res.status}`);
+        return res.json();
+      })
+      .then(data => {
+        console.log("Webhook success:", data);
+      })
+      .catch(err => {
+        console.error("Webhook error:", err);
+      });
+    }, [customer_number]);
 
   return (
     <Suspense fallback={<LoadingAnimation/>}>
       <Fragment>
-        <Landpage name={name} /> {/* Pass name as prop */}
+        <Landpage name={userName} /> {/* Pass name as prop */}
         <About />
         <Speaker />
-        <Benifit name={name}/>
+        <Benifit name={userName}/>
         <Footer />
       </Fragment>
     </Suspense>
